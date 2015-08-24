7d0d48039e6643a9937386cd8035633431f8c82f
Mike Jones <mikethespike77@gmail.com>
2015-08-24 14:34:35 +0100

    figured out how to assign health to each tank and make attacks assign values

--- js/script.js
+++ js/script.js
@@ -1,46 +1,72 @@
 $(document).ready(function() {
 
+var tankChecker;
+var tankHealth1 = [10];//for assigning a health value to 1st tank
+var tankHealth2 = [10];//for assigning a health value to 2nd tank
+var attack = function(){
+	return Math.round(Math.random()*5)
+}
+var tank1Attacked = function(){
+	var attacked = tankHealth1[0]-attack();
+    tankHealth1.unshift(attacked);
+    console.log(tankHealth1[0])
+}
+
+var tank2Attacked = function(){
+	var attacked = tankHealth2[0]-attack();
+    tankHealth2.unshift(attacked);
+    console.log(tankHealth1[0])
+}
+
+
+
 console.log("hello");
+// colission detection for the two tanks
+var colided = function(){
+	if ($('.team1').position().left >= $('.team2').position().left){
+		console.log('tanks have colided');
+		clearInterval(tankChecker);
+	}
+}
+
 
 // getting shit to do shit
 $('body').on('keypress', function(e){
 	console.log(e.keyCode);
 	switch(e.keyCode){
 	case 119:
+	console.log('w has been clicked');
 	$('body').append('<img class="team1" src="images/king_tiger.gif">');
     tankTeam1Right();
+    // tankChecker = setInterval(colided, 50);
     break;
     case 108:
     console.log('L has been clicked');
     $('body').append('<img class="team2" src="images/tank_2.gif">');
     tankTeam2Right();
+   tankChecker = setInterval(colided, 50);
     break;
     default:
     console.log('press the right button');
    }
   });
-// getting second shit to do shit
-// $('#l').on('l', function(){
-	
-// });
-
-
 // to make tank left to move right
-function tankTeam1Left() {
-    $(".team1").animate({left: "-=500"}, 2000, "swing", tankTeam1Right);
-}
 function tankTeam1Right() {
-    $(".team1").animate({left: "+=500"}, 2000, "swing", tankTeam1Left);
+    $(".team1").animate({left: "+=1100"}, 2000, "swing");
 }
-
 
 // to make tank right to move left
-function tankTeam2Left() {
-    $(".team2").animate({right: "-=500"}, 2000, "swing", tankTeam2Right);
-}
 function tankTeam2Right() {
-    $(".team2").animate({right: "+=500"}, 2000, "swing", tankTeam2Left);
+    $(".team2").animate({right: "+=1100"}, 2000, "swing");
 }
+
+
+
+
+
+
+
+
 
 
 

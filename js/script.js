c94c5ed25810bc2808cc552780c980b9e3f534c8
Mike Jones <mikethespike77@gmail.com>
2015-08-24 15:13:49 +0100

    got the tanks to stop when they collide

--- js/script.js
+++ js/script.js
@@ -6,12 +6,13 @@ var tankHealth2 = [10];//for assigning a health value to 2nd tank
 var attack = function(){
 	return Math.round(Math.random()*5)
 }
+// what happens when tank 1 is attacked
 var tank1Attacked = function(){
 	var attacked = tankHealth1[0]-attack();
     tankHealth1.unshift(attacked);
     console.log(tankHealth1[0])
 }
-
+// what happens when tank 2 is attacked
 var tank2Attacked = function(){
 	var attacked = tankHealth2[0]-attack();
     tankHealth2.unshift(attacked);
@@ -21,11 +22,13 @@ var tank2Attacked = function(){
 
 
 console.log("hello");
-// colission detection for the two tanks
+// colission detection for the two tanks and get them to stop
 var colided = function(){
 	if ($('.team1').position().left >= $('.team2').position().left){
 		console.log('tanks have colided');
 		clearInterval(tankChecker);
+		$('.team1').stop();
+		$('.team2').stop();
 	}
 }
 
@@ -44,7 +47,7 @@ $('body').on('keypress', function(e){
     console.log('L has been clicked');
     $('body').append('<img class="team2" src="images/tank_2.gif">');
     tankTeam2Right();
-   tankChecker = setInterval(colided, 50);
+   tankChecker = setInterval(colided, 10);
     break;
     default:
     console.log('press the right button');
@@ -52,12 +55,12 @@ $('body').on('keypress', function(e){
   });
 // to make tank left to move right
 function tankTeam1Right() {
-    $(".team1").animate({left: "+=1100"}, 2000, "swing");
+    $(".team1").animate({left: "+=1100"}, 5000, "swing");
 }
 
 // to make tank right to move left
 function tankTeam2Right() {
-    $(".team2").animate({right: "+=1100"}, 2000, "swing");
+    $(".team2").animate({right: "+=1100"}, 5000, "swing");
 }
 
 

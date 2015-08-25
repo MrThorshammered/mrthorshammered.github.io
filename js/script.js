3212f9cceeefcef99d2de1879b170162c33d8408
Mike Jones <mikethespike77@gmail.com>
2015-08-25 12:50:03 +0100

    final logic working

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-var $cached_elem1 = $('.team1');
-var $cached_elem2 = $('.team2');
+var building1Timer;
+var building2Timer;
 var tankOneTimer;
 var tankTwoTimer;
 var tankChecker1;
 var tankChecker2;
-var buildingHealth1 = [50];
-var buildingHealth2 = [50];
+var buildingHealth1 = [50];//assigning a health value to 1st garage 
+var buildingHealth2 = [50];//assigning a health value to 2nd garage
 var tankHealth1 = [10];//for assigning a health value to 1st tank
 var tankHealth2 = [10];//for assigning a health value to 2nd tank
 var attack1 = function(){//how the attack assigns a value to itself
@@ -18,19 +18,31 @@ var attack2 = function(){//how the attack assigns a value to itself
 
 // timer to check who's won the battle
 var didTankOneWin = function(){
-    tankOneTimer = setInterval(tank1Attacked,2000);
+    tankOneTimer = setInterval(tank1Attacked,1000);
 };
 var didTankTwoWin = function(){
-    tankTwoTimer = setInterval(tank2Attacked,2000);
+    tankTwoTimer = setInterval(tank2Attacked,1000);
+};//to check if the tank has hit a building
+var hasABuildingBeenHit = function(team){
+    building1Timer = setInterval(function(){
+        buildingColide(team)
+    },50);
 };
+var buildingIsBeingAttacked = function(){
+  building2Timer = setInterval(buildingAttack1,1000);
+}
+
 // what happens when tank 1 is attacked
 var tank1Attacked = function(){
 	var attacked = tankHealth1[0]-attack2();
     tankHealth1.unshift(attacked);
     console.log(tankHealth1[0] + " tank ones health");
+    $('h4#tankOneComentary').html(tankHealth1[0] + " tank health left");
     if (tankHealth1[0] <= 0){
     	$('.team1').remove();
         tankTeam2Right();
+        $('h4').html('tank 2 survived');
+        hasABuildingBeenHit($('.team2'));
         clearInterval(tankOneTimer);
         clearInterval(tankTwoTimer);
     }//else if($('.team2').remove() === true){
@@ -43,15 +55,15 @@ var tank2Attacked = function(){
 	var attacked = tankHealth2[0]-attack1();
     tankHealth2.unshift(attacked);
     console.log(tankHealth2[0] + " tank two's health");
+    $('h4#tankTwoComentary').html(tankHealth2[0] + " tank health left");
     if (tankHealth2[0] <= 0){
     	$('.team2').remove();
         tankTeam1Right();
+        $('h4.whoSurvived').html('tank 1 survived');
+        hasABuildingBeenHit($('.team1'));
         clearInterval(tankOneTimer);
         clearInterval(tankTwoTimer);
-    }//else if($('.team1').remove() === true){
-    //     debugger;
-    //     tankTeam2Right();
-    //  }
+    }
 };
 
 // colission detection for the two tanks and get them to stop
@@ -67,16 +79,56 @@ var colided = function(){
         didTankTwoWin();
 	}
 };
-
-var buildingColide = function(){
-    if($('.team2').position().left >= $('#garage1').position().left){
-        console.log('tank two is attacking');
-        $('.team2').stop();
-    }else if($('.team1').postion().left >= $('#garage2').postion().left){
-        console.log('tank one is attacking');
-        $('.team1').stop();
-    }
+// checking to see if tank has stopped at buildings
+var buildingColide = function(team){
+    
+    if(team.position().left <= 200 || team.position().left > window.innerWidth-200){
+      console.log('tank TBD is attacking');
+      team.stop();
+      clearInterval(building1Timer);
+      buildingAttack1();
+     buildingIsBeingAttacked();
+    };
+    
+    
 };
+//buildings being attacked
+var buildingAttack1 = function(team){
+  if($('h4').html() === "tank 1 survived"){
+    var attacked1 = buildingHealth2[0]-attack1();
+    buildingHealth2.unshift(attacked1);
+    console.log(buildingHealth2[0] + " building two's health");
+    $('h4#tankTwoComentary').html(buildingHealth2[0] + " building health left");
+    if (buildingHealth2[0] <= 0){
+      $('img#garage2').remove()
+      clearInterval(building2Timer);
+      $('h4.whoSurvived').html("team 1 won mofo");
+    }
+  }else if($('h4').html() === "tank 2 survived"){
+  var attacked2 = buildingHealth1[0]-attack2();
+  // debugger;
+    buildingHealth1.unshift(attacked2);
+    console.log(buildingHealth1[0] + " building one's health");
+    $('h4#tankOneComentary').html(buildingHealth1[0] + " building health left");
+    if(buildingHealth1[0] <= 0){
+      $('img#garage1').remove()
+      clearInterval(building2Timer);
+      $('h4.whoSurvived').html("team 2 won mofo");
+    }
+  }
+};
+
+// var buildingAttack2 = function(team){
+//   if($('team1').position() === undefined){
+//       console.log('tank one destroyed');
+//   }else if(team.position().left <= 200){
+//   var attacked2 = buildingHealth1[0]-attack2;
+//     buildingHealth1.unshift(attacked2);
+//     console.log(buildingHealth1);
+//   }
+// };
+
+
 
 
 // getting shit to do shit
